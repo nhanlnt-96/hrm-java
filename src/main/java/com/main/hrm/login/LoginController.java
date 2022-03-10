d08b442c407da8ed52b41654a827ee916de53d8e
nhanlnt-96 <nhanlnt@hotmail.com>
2022-03-11 00:46:45 +0700

    loginView: update login view

--- src/main/java/com/main/hrm/login/LoginController.java
+++ src/main/java/com/main/hrm/login/LoginController.java
@@ -1,22 +1,35 @@
 package com.main.hrm.login;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.net.URL;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+
   @FXML
   private ImageView login_bg;
+
+  @FXML
+  private TextField usernameInput;
+
+  @FXML
+  private PasswordField passwordInput;
+
+  @FXML
+  private Button loginBtn;
+
+  @FXML
+  private Button cancelBtn;
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -25,4 +38,8 @@ public class LoginController implements Initializable {
     login_bg.setImage(image);
   }
 
+  public void onCancelBtnClick(ActionEvent actionEvent) {
+    Stage stage = (Stage) cancelBtn.getScene().getWindow();
+    stage.close();
+  }
 }
