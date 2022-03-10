4ac589f0ef27393e58674e0b979d2222757d7265
nhanlnt-96 <nhanlnt@hotmail.com>
2022-03-11 00:30:54 +0700

    loginView: complete create login view

--- src/main/java/com/main/hrm/login/LoginController.java
+++ src/main/java/com/main/hrm/login/LoginController.java
@@ -1,14 +1,28 @@
 package com.main.hrm.login;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class LoginController {
-  @FXML
-  private Label welcomeText;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
+public class LoginController implements Initializable {
   @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
+  private ImageView login_bg;
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    File file = new File("src/main/imgs/login-bg.png");
+    Image image = new Image(file.toURI().toString());
+    login_bg.setImage(image);
   }
+
 }
